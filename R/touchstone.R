c17e3b38f52ca08b2d1bfe651ca1ea5eda526faa
Wes Hinsley <w.hinsley@imperial.ac.uk>
2020-05-20 13:45:41 +0100

    Remove always-false condition in touchstone.R extract

--- R/touchstone.R
+++ R/touchstone.R
@@ -15,10 +15,6 @@ extract_touchstone <- function(e, path, con) {
   # Query DB for all touchstones that are connected with touchstones
   # or touchstone names
 
-  if (length(ts) == 0) {
-    return(eout)
-  }
-
   ts <- c(ts,
     DBI::dbGetQuery(con, sprintf("
       SELECT DISTINCT touchstone.id
