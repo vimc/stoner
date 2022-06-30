8c518ed4b6f6008e302bdb4b6fd01e2b054d1785
Robert Ashton <r.ashton@imperial.ac.uk>
2022-06-30 15:58:55 +0100

    Fix order in dplyr rename command

--- R/stochastic_process.R
+++ R/stochastic_process.R
@@ -246,9 +246,9 @@ aggregate_data <- function(scenario_data) {
   scen_coh <- agg_and_sort(scenario_data)
 
   scen_u5_coh <- scen_u5_coh %>%
-    dplyr::rename(year = cohort)
+    dplyr::rename(cohort = year)
   scen_coh <- scen_coh %>%
-    dplyr::rename(year = cohort)
+    dplyr::rename(cohort = year)
 
   list(
     u5_calendar_year = scen_u5_cal,
