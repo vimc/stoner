4023eb58e5e1dd49cf6dfac695899c612c5d4b67
Wes Hinsley <w.hinsley@imperial.ac.uk>
2026-03-02 14:46:11 +0000

    Calculate/test central

--- tests/testthat/test_stochastic_files.R
+++ tests/testthat/test_stochastic_files.R
@@ -4,13 +4,13 @@ fake_data <- function() {
   data.frame(
     disease = "elf-piles",
     country = "LAP",
-    year = rep(2000:2005, each = 5),
-    age = rep(0:5, 5),
-    cases = 1:30,
+    year = rep(2000:2005, each = 6),
+    age = rep(0:5, 6),
+    cases = 1:36,
     cohort_size = c(100, 200, 300, 400, 500, 600),
-    deaths = 31:60,
-    dalys = 61:90,
-    yll = 91:120,
+    deaths = 31:66,
+    dalys = 61:96,
+    yll = 91:126,
     run_id = 1)
 }
 
@@ -90,9 +90,60 @@ test_that("Standardise works with missing run_id column", {
 
   pq <- arrow::read_parquet(file.path(tmpout, "north_pole_fat_LAP.pq"))
   tab <- table(pq$run_id)
-  expect_all_equal(as.integer(tab), 30)
+  expect_all_equal(as.integer(tab), 36)
   expect_equal(length(tab), 200)
   expect_true(all.equal(names(tab), as.character(1:200)))
 })
 
 
+test_that("Create central - bad path / files", {
+
+  expect_error(stone_stochastic_central("P:/potato", "", "", "", ""),
+               "Path not found:")
+
+  tmp <- tempdir()
+  path <- file.path(tmp, "touchstone", "disease_group")
+  dir.create(path, showWarnings = FALSE, recursive = TRUE)
+  expect_error(stone_stochastic_central(tmp, "touchstone", "disease", "group",
+                                        "scenario"),
+               "No .pq files found")
+
+  file.create(file.path(path, "potato.pq"), showWarnings = FALSE)
+
+  expect_error(stone_stochastic_central(tmp, "touchstone", "disease", "group",
+                                        "scenario"),
+               "No files matching scenario")
+})
+
+test_that("Create central works", {
+  df1 <- fake_data()
+  df2 <- df1
+  df2$deaths <- df2$deaths * 2
+  df2$run_id <- 2
+  df1 <- rbind(df1, df2)
+
+  df2 <- df1
+  df2$country <- "NPL"
+  df2$cases <- df2$cases * 2
+
+  tmpdir <- tempdir()
+  path <- file.path(tmpdir, "tc", "piles_northpole")
+  dir.create(path, showWarnings = FALSE, recursive = TRUE)
+
+  arrow::write_parquet(df1, file.path(path, "northpole_sc1_LAP.pq"))
+  arrow::write_parquet(df2, file.path(path, "northpole_sc1_NPL.pq"))
+  stone_stochastic_central(tmpdir, "tc", "piles", "northpole", "sc1")
+  expect_true(file.exists(file.path(path, "northpole_sc1_central.pq")))
+
+  central <- as.data.frame(
+    arrow::read_parquet(file.path(path, "northpole_sc1_central.pq")))
+  expect_false("run_id" %in% names(central))
+  expect_true(length(unique(central$country)) == 2)
+  p1 <- central[central$year == 2002 & central$age == 3, ]
+
+  expect_true(p1$deaths[p1$country == "LAP"] ==
+        (df1$deaths[df1$year == 2002 & df1$age == 3 & df1$run_id == 1] +
+         df1$deaths[df1$year == 2002 & df1$age == 3 & df1$run_id == 2]) / 2)
+
+  expect_true(p1$cases[p1$country == "NPL"] == 2 * p1$cases[p1$country == "LAP"])
+})
